a6e8821cb14e6210f8f25a057c69744357a879e7
mdtamimkhan842 <mdtamimkhan842@gmail.com>
2022-10-13 23:10:48 +0600

    first commit

---
+++ Better Passwords in c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+char A[500];
+int i, j, length;
+
+gets(A);
+
+length=strlen(A);
+length >= 16
+
+
+return 0;
+}
---
+++ Let’s Build Nepal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+    int i, j, k, T, length;
+    char S[500];
+
+    scanf("%d", &T);
+
+    for (i=1; i<=T; i++)
+    {
+        gets(S);
+
+
+    for (i=0; S[i] != '\0'; i++)
+    {
+        if (!(S[i] == ' ' && S[i] != '0'))
+        {
+        }
+    }
+    printf("Maile Nepal banauna sakchhu!!\n");
+
+
+    for (i=0; S[i] != '\0'; i++)
+    {
+        if (S[i] == ' ' && S[i] != '0')
+        {
+        }
+    }
+
+    printf("Hami sabai milera Nepal Banau hai!!\n");
+    }
+
+    return 0;
+}
